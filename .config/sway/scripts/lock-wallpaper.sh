4faea7203cd50aaf90e3bec7a5c31f287d9bea0a
madwayl <67217921+madwayl@users.noreply.github.com>
2025-05-02 22:32:42 +0530

    misc changes

--- .config/sway/scripts/lock-wallpaper.sh
+++ .config/sway/scripts/lock-wallpaper.sh
@@ -4,12 +4,11 @@
 # and applies a Gaussian blur effect to it, saving the result as /tmp/lockscreen.png.
 # It uses the `magick` command from ImageMagick to perform the image processing.
 
-wallpaper_choice=$(find $HOME/.local/share/wallpapers -type f | shuf -n 1)
+wallpaper_choice=$(find $HOME/.dotfiles/.local/share/wallpapers -type f | shuf -n 1)
 
-cp $wallpaper_choice /home/mzaql/.local/share/wallpapers/tmp/wallpaper.png
+cp $wallpaper_choice $HOME/.local/share/wallpapers/tmp/wallpaper.png
 cp $wallpaper_choice /usr/share/sddm/tmp/wallpaper.png
 
+/usr/bin/magick $HOME/.local/share/wallpapers/tmp/wallpaper.png -filter Gaussian -resize 20% -blur 0x2.5 /tmp/lockscreen.png
 
-magick /home/mzaql/.local/share/wallpapers/tmp/wallpaper.png -filter Gaussian -resize 20% -blur 0x2.5 /tmp/lockscreen.png
-
-swaymsg output "*" bg ~/.local/share/wallpapers/tmp/wallpaper.png fill
+swaymsg output "*" bg $HOME/.local/share/wallpapers/tmp/wallpaper.png fill
