d770e229dfc0f0aa0b9116f5aa177835d364657e
madwayl <67217921+madwayl@users.noreply.github.com>
2025-05-12 22:03:36 +0530

    Org + Misc.

--- sway/.config/sway/scripts/lock-wallpaper.sh
+++ sway/.config/sway/scripts/lock-wallpaper.sh
@@ -4,11 +4,13 @@
 # and applies a Gaussian blur effect to it, saving the result as /tmp/lockscreen.png.
 # It uses the `magick` command from ImageMagick to perform the image processing.
 
-wallpaper_choice=$(find $HOME/.dotfiles/.local/share/wallpapers -type f | shuf -n 1)
+wallpaper_choice=$(find ~/.dotfiles/.local/share/wallpapers -type f | shuf -n 1)
 
 cp $wallpaper_choice $HOME/.local/share/wallpapers/tmp/wallpaper.png
 cp $wallpaper_choice /usr/share/sddm/tmp/wallpaper.png
 
-/usr/bin/magick $HOME/.local/share/wallpapers/tmp/wallpaper.png -filter Gaussian -resize 20% -blur 0x2.5 /tmp/lockscreen.png
+/usr/bin/magick $wallpaper_choice -filter Gaussian -resize 20% -blur 2x20 /tmp/lockscreen.png
 
-swaymsg output "*" bg $HOME/.local/share/wallpapers/tmp/wallpaper.png fill
+# export WAYLAND_DISPLAY="wayland-1"
+~/.nix-profile/bin/swww query || ~/.nix-profile/bin/swww-daemon
+~/.nix-profile/bin/swww img $wallpaper_choice
